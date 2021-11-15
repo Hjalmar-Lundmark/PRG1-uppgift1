755bf45d1dc32feb8418ab7f39e05631d1e078e4
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2021-11-15 11:17:22 +0100

    Started

--- src/Medelvärde.java
+++ src/Medelvärde.java
@@ -1,5 +1,17 @@
+import javax.swing.*;
+import java.util.Scanner;
+
 public class Medelvärde {
     public static void main(String[] args) {
-        
+        String antalS = JOptionPane.showInputDialog("Hur många tal matas in?");
+        int antal = Integer.parseInt(antalS);
+        int[] lista = new int[antal];
+        Scanner tgb = new Scanner(System.in);
+
+        for (int i = 0; i < lista.length; i++ ) {
+            lista[i] = tgb;
+        }
+
+
     }
 }
