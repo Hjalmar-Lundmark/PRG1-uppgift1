ed3f85d53e83a8a2dd9a581077a7b6abc4b5822d
Hjalmar-Lundmark <hjalmar.lundmark@elev.ga.ntig.se>
2021-11-16 15:09:16 +0100

    Nästan klar med "Medelvärde"

--- src/Medelvärde.java
+++ src/Medelvärde.java
@@ -7,11 +7,35 @@ public class Medelvärde {
         int antal = Integer.parseInt(antalS);
         int[] lista = new int[antal];
         Scanner tgb = new Scanner(System.in);
+        int summa = 0;
 
+        System.out.println("Skriv in talen");
         for (int i = 0; i < lista.length; i++ ) {
-            lista[i] = tgb;
+            lista[i] = tgb.nextInt();
+            summa = summa + lista[i];
+        }
+        float medel = summa;
+        medel / lista.length;
+        System.out.println(medel);
+
+        int index = 0;
+        int störsthittils = 4;
+        while (index < lista.length){
+            if (störsthittils < lista[index]);{
+                störsthittils = lista[index];
+            }
+            index++;
         }
 
+        int index2 = 0;
+        int minsthittils = 4;
+        while (index2 < lista.length){
+            if (minsthittils > lista[index2]);{
+                minsthittils = lista[index2];
+            }
+            index2++;
+        }
 
+        System.out.println("Största: " + störsthittils + "\nMinsta: " + minsthittils);
     }
 }
---
+++ src/uppgift2.java
@@ -0,0 +1,6 @@
+public class uppgift2 {
+    public static void main(String[] args) {
+        //tom sträng + nextChar
+        //Upp3: ge värden, #d-#a = x
+    }
+}
